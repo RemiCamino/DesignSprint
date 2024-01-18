875802798b5937e80e6ba99a40a594497451bc55
rems <remi.camino@epitech.eu>
2024-01-18 10:40:53 +0100

    easy build

---
+++ build.ps1
@@ -0,0 +1,3 @@
+# PowerShell script to build and start services defined in docker-compose.yml
+docker-compose build
+docker-compose up
---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Shell script to build and start services defined in docker-compose.yml
+docker-compose build
+docker-compose up
